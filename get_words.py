7b9036027d2c202ba890ba53fa6f7be81d4f054e
whoiam143 <150559715+whoiam143@users.noreply.github.com>
2023-11-27 14:21:23 +0000

    Update get_words.py

--- get_words.py
+++ get_words.py
@@ -14,21 +14,4 @@ class Text:
             shuffle(self.list_of_words)
             for i in range(self.numbers_of_words):
                 self.text.append(choice(self.list_of_words))
-
-    def get_string(self):
-        strings = []
-        self.get_some_words()
-        if self.numbers_of_words == 5 or self.numbers_of_words == 15:
-            return self.text
-        elif self.numbers_of_words == 25:
-            strings.append(list(self.text)[:10])
-            strings.append(list(self.text)[10:20])
-            strings.append(list(self.text)[20:])
-            return strings
-        elif self.numbers_of_words == 25:
-            strings.append(list(self.text)[:10])
-            strings.append(list(self.text)[10:20])
-            strings.append(list(self.text)[20:30])
-            strings.append(list(self.text)[30:40])
-            strings.append(list(self.text)[40:])
-            return strings
+        return self.text
