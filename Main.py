ba63cfd0476dacdd9f63dc1f965258e294c86230
whoiam143 <150559715+whoiam143@users.noreply.github.com>
2023-11-27 14:20:57 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@
 import sys
 
 
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
 from PyQt5.QtGui import QKeyEvent, QMovie
 from PyQt5.QtCore import QTimer
 from PyQt5 import uic
@@ -14,16 +14,16 @@ from get_words import Text
 class MainWin(QMainWindow):
     def __init__(self):
         super(MainWin, self).__init__()
+        self.setFixedSize(1360, 770)
         self.TEXT = ""
         self.ui = uic.loadUi("1.ui", self)
         self.start_ui()
 
-        self.number_of_words = 25
+        self.number_of_words = 5
 
         self.text = Text(self.number_of_words)
-        self.x = self.text.get_string()
+        self.x = self.text.get_some_words()
 
-        self.insert_text(self.number_of_words)
         self.counter = 0
         self.sec = -1
         self.minut = 0
@@ -32,8 +32,8 @@ class MainWin(QMainWindow):
         self.timer.timeout.connect(self.update_time)
         self.stop_button.clicked.connect(self.stop_time)
         self.reset_button.clicked.connect(self.reset_time)
+        self.inserted_text.setText(self.x[self.counter])
 
-        self.mistakes_count = 0
         #self.gifka = QMovie("bongocat.gif")
         #self.gif.setMovie(self.gifka)
         #self.gifka.start()
@@ -46,14 +46,15 @@ class MainWin(QMainWindow):
         text = event.text()
         if len(self.TEXT) != 0:
             if text == "\r":
-                if not self.mistakes.isChecked():
-                    if self.check(self.x, self.TEXT, self.counter):
-                        self.counter += 1
-                        self.TEXT = ""
-                else:
-                    self.check(self.x, self.TEXT, self.counter)
+                if self.check(self.x, self.TEXT, self.counter):
                     self.counter += 1
                     self.TEXT = ""
+                    try:
+                        self.inserted_text.setText(self.x[self.counter])
+                    except IndexError:
+                        self.messege_box()
+                        self.stop_time()
+
             if text == "\x08":
                 self.TEXT = self.TEXT[:-1]
                 self.input_text.setText(self.TEXT)
@@ -66,41 +67,10 @@ class MainWin(QMainWindow):
             self.input_text.setText(self.TEXT)
 
     def check(self, string, current_word, index_of_word):
-        if self.number_of_words < 25:
-            if not self.mistakes.isChecked():
-                return string[index_of_word] == current_word.strip()
-            else:
-                for letters in range(len(current_word.strip())):
-                    if current_word[letters] != string[index_of_word][letters]:
-                        self.mistakes_count += 1
-        elif self.number_of_words == 25:
-            if not self.mistakes.isChecked():
-                current_list = 0
-                if self.counter // 10 > current_list:
-                    current_list += 1
-                    index_of_word -= 10
-                    #self.counter -= 10
-                    print(current_list, index_of_word, self.counter)
-                return string[current_list][index_of_word] == current_word.strip()
+        print(self.x)
 
+        return string[index_of_word] == current_word.strip()
 
-#
-#
-#
-        #elif self.number_of_words == 50:
-        #    pass
-
-
-    def insert_text(self, words):
-        if words < 25:
-            s = " ".join([str(i) for i in self.x])
-            self.inserted_text.setText(s)
-        if words == 25 or words == 50:
-            s = ""
-            for i in self.x:
-                s += F'{" ".join(i)}\n'
-            self.inserted_text.setText("".join(s))
-            print(self.x)
 
     def update_time(self):
         self.sec += 1
@@ -126,6 +96,14 @@ class MainWin(QMainWindow):
     def reset_time(self):
         self.sec = 0
         self.minut = 0
+
+    def messege_box(self):
+        let = [len(i) for i in self.x]
+        wpm = str(int((self.number_of_words * 60) / ((self.minut * 60) + self.sec)))
+        box = QMessageBox.information(self, "Result", F"You result is {wpm} wpm", QMessageBox.Ok, QMessageBox.Ok)
+        print(sum(let) / 5)
+        print(let)
+        print(box)
 
 
 if __name__ == "__main__":
